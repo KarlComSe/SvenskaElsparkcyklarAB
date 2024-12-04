a6834841c90ac22432344fd68d54e4ed42c4705f
owsu23 <owsu23@student.bth.se>
2024-12-04 10:44:33 +0100

    Added CORS policy to allow for swagger/frontend-connections

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,16 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
+    app.enableCors({
+      origin: [
+        'http://localhost:5173',    // Frontend
+        `http://localhost:3535${process.env.PORT ?? 3000}`,    // Swagger UI
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+      credentials: true,
+    });
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
