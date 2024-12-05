5cc9d2e279c21691eee78c4d29572b8a02dffa80
Pbris <peter.landbris@gmail.com>
2024-12-05 15:49:44 +0100

    Added routes in controller for 'show all', 'show 1' and 'update 1'. Specified in update-user.dto what format updates should be.

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { NotFoundException } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -21,6 +22,31 @@ export class UsersService {
         }
 
         user.hasAcceptedTerms = hasAcceptedTerms;
+        return this.userRepository.save(user);
+    }
+    // Find all customers
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+    // Find a customer by ID
+    async findById(githubId: string): Promise<User> {
+        const user = await this.userRepository.findOne({ where: { githubId } });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
+    }
+    // Update customer fields
+    async update(githubId: string, updateUserDto: UpdateUserDto): Promise<User> {
+        const user = await this.findById(githubId);
+
+        // Check if its allowed in user-dto
+        if (updateUserDto.email) user.email = updateUserDto.email;
+        if (updateUserDto.roles) user.roles = updateUserDto.roles;
+        if (updateUserDto.hasAcceptedTerms !== undefined) {
+            user.hasAcceptedTerms = updateUserDto.hasAcceptedTerms;
+        }
+
         return this.userRepository.save(user);
     }
 }
