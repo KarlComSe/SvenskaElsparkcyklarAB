b81a6cd3842d9b24d18a9e277ef7cbe5f5a41c3e
owsu23 <owsu23@student.bth.se>
2024-12-08 09:33:15 +0100

    Added testing for redux components, hopefully

--- frontend/src/components/Header.test.tsx
+++ frontend/src/components/Header.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Header from './Header';
+import {renderWithProviders } from '../helpers/test-redux';
 
 describe('Header', () => {
   it('renders Header component', () => {
-    render(<Header/>);
+    render(renderWithProviders(<Header/>));
   });
 });
+
+
