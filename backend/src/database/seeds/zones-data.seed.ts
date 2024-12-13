874e8468bb3564f3bcc29305dceb49fabf745178
Pbris <peter.landbris@gmail.com>
2024-12-13 17:02:51 +0100

    Adjusted example data for more realistic zonez.

--- backend/src/database/seeds/zones-data.seed.ts
+++ backend/src/database/seeds/zones-data.seed.ts
@@ -11,24 +11,24 @@ export default class ZoneSeeder {
                 {
                     polygon: [
                         { lat: 59.3293, lng: 18.0686 },
-                        { lat: 59.3294, lng: 18.0687 },
-                        { lat: 59.3295, lng: 18.0688 }
+                        { lat: 59.3298, lng: 18.0687 },
+                        { lat: 59.3303, lng: 18.0688 }
                     ],
                     type: 'parking'
                 },
                 {
                     polygon: [
                         { lat: 59.8586, lng: 17.6389 },
-                        { lat: 59.8587, lng: 17.6390 },
-                        { lat: 59.8588, lng: 17.6391 }
+                        { lat: 59.8591, lng: 17.6390 },
+                        { lat: 59.8596, lng: 17.6391 }
                     ],
                     type: 'charging'
                 },
                 {
                     polygon: [
-                        { lat: 58.4108, lng: 15.6214 },
+                        { lat: 58.4100, lng: 15.6214 },
                         { lat: 58.4109, lng: 15.6215 },
-                        { lat: 58.4110, lng: 15.6216 }
+                        { lat: 58.4115, lng: 15.6216 }
                     ],
                     type: 'speed',
                     speedZone: {
