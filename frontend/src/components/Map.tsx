cdf05da20b6e16a534d9e1497332ffc0ca6ddd85
owsu23 <owsu23@student.bth.se>
2024-12-16 21:08:39 +0100

    Bike positions are retrieved from API

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,23 +1,46 @@
 import { MapContainer, Popup, Marker, TileLayer, Polygon, useMap } from 'react-leaflet';
 import React, { useEffect, useState } from 'react';
 import { LatLngTuple,  LatLngExpression } from 'leaflet';
-import L from 'leaflet';
-import markerIcon from '../assets/images/station.png';
-import { API_URL, getHeader } from '../helpers/config';
+import { API_URL, getHeader, iconStation } from '../helpers/config';
 import axios from 'axios';
 import { RootState } from '../redux/store/store';
 import { useDispatch, useSelector } from 'react-redux';
 
-export default function Map() {
-    
-    const [startPosition, setStartPosition] = useState<LatLngExpression>([51.505, -0.09]);
-    const {isLoggedIn, token, user, role} = useSelector((state: RootState) =>  state.auth);
+type Scooter = {
+    id: string;
+    batteryLevel: number;
+    latitude: number;
+    longitude: number;
+    status: string;
+  };
 
-    const scooterPositions: LatLngTuple[] = [
-    [51.505, -0.09],
-    [51.515, -0.1],
-    [51.525, -0.08]
-    ];
+type PolygonPoint = {
+    lat: number;
+    lng: number;
+  };
+
+type SpeedZone = {
+    id: string;
+    speedLimit: number;
+};
+
+type Zone = {
+    id: string;
+    polygon: PolygonPoint[];
+    type: 'parking' | 'charging' | 'speed';
+    speedZone?: SpeedZone | null;
+};
+
+export default function Map() {
+    const [startPosition, setStartPosition] = useState<LatLngExpression>([59.2741, 15.2066]);
+    const {isLoggedIn, token, user, role} = useSelector((state: RootState) =>  state.auth);
+    const [scooterData, setScooterData] = useState<Scooter[]>([]);
+    const [zoneData, setZoneData] = useState<Zone[]>([]);
+    // const scooterPositions: LatLngTuple[] = [
+    // [51.505, -0.09],
+    // [51.515, -0.1],
+    // [51.525, -0.08]
+    // ];
 
     const stationPositions: LatLngTuple[] = [
         [51.505, -0.04],
@@ -37,14 +60,16 @@ export default function Map() {
           [51.53, -0.07],
         ],
     ];
+    const purpleOptions = { color: 'purple' }
 
     useEffect(() => {
         const fetchScooters = async() => {
         try {
     
-                const response = await axios.get(`${API_URL}/bike`, getHeader(token));
+                const response = await axios.get(`${API_URL}/bike`);
                 console.log(response.data);
-
+                const positions: LatLngTuple[] = [];
+                setScooterData(response.data);
             }
             catch(error)
             {
@@ -54,28 +79,43 @@ export default function Map() {
       fetchScooters();
       },[])
     
-
-    const iconStation = new L.Icon({
-        iconUrl: markerIcon,
-        iconSize:     [38, 38], // size of the icon
-        shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76], // point from which the popup should open relative to the iconAnchor
-    });
+      useEffect(() => {
+        const fetchZones = async() => {
+        try {
     
-    const purpleOptions = { color: 'purple' }
-  
+                const response = await axios.get(`${API_URL}/zone`);
+                console.log(response.data);
+                const positions: LatLngTuple[] = [];
+                for (let key in response.data) {
+                    if (response.data.hasOwnProperty(key)) {
+                        // const latitude = response.data[key].latitude;
+                        // const longitude = response.data[key].longitude;
+                        // positions.push([latitude, longitude]);
+                        // console.log(key, latitude, longitude);
+                    }
+                 }
+                //  setScooterPosition(positions);
+            }
+            catch(error)
+            {
+                console.log(error);
+            }
+      }
+      fetchZones();
+      },[])
+
+    
     const renderScooterMarkers = () => (
-        scooterPositions?.map((position, index) => (
-        <Marker key={index} position={position}>
+        scooterData?.map((scooter, index) => (
+        <Marker key={index} position={[scooter.latitude, scooter.longitude]}>
             <Popup>
-            { position }
+            <p>Id: { scooter.id} </p>
+            <p>BatteryLevel: { scooter.batteryLevel} </p>
+            <p>Status: { scooter.status} </p>
             </Popup>
         </Marker>))
         );
 
-        
     const renderStationMarkers = () => (
         stationPositions?.map((position, index) => (
         <Marker key={index} position={position} icon={iconStation}>
@@ -87,7 +127,7 @@ export default function Map() {
 
   return (
     <div id="map" data-testid="map">
-        <MapContainer style={{ height: "400px" }}  center={startPosition} zoom={13} scrollWheelZoom={false}>
+        <MapContainer style={{ height: "400px" }}  center={startPosition} zoom={6} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
