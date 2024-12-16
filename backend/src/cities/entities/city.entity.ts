cd43f892b08b18d8a8250a3993ffd4ccc52c5dc4
KarlComSe <karl@wackerberg.se>
2024-12-16 18:33:15 +0100

    add city entity

---
+++ backend/src/cities/entities/city.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class City {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ 
+        type: 'simple-enum', 
+        enum: ['Stockholm', 'Linköping', 'Uppsala'], 
+        default: 'Stockholm'
+    })
+    status: 'Stockholm' | 'Linköping' | 'Uppsala';
+
+    @Column('float', { nullable: true })
+    latitude?: number;
+    longitude?: number;
+
+    @CreateDateColumn({ nullable: true })
+    createdAt: Date;
+
+    @UpdateDateColumn({ nullable: true })
+    updatedAt: Date;
+}
